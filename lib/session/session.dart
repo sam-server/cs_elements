369548c52dbfc535781d920b600ad6a23fdf65a5
Thomas Stephenson <ovangle@gmail.com>
2014-12-15 00:39:42 +1100

    Session element retrieves auth token from cookies

--- lib/session/session.dart
+++ lib/session/session.dart
@@ -17,6 +17,11 @@ import 'package:cookies/cookies.dart';
 import '../context_panel/context_panel.dart';
 
 /**
+ * Gets the session from the body of the html (if one is present)
+ */
+SessionElement get session => querySelector('cs-session');
+
+/**
  * A [SessionElement] defines various global parameters
  * to the application.
  */
@@ -25,24 +30,24 @@ class SessionElement extends PolymerElement {
   
   CookieJar _cookies;
   
+  void _saveCookies() {
+    document.cookie = _cookies.values.map((v) => '$v').join(';');
+  }
+  
+  bool get loggedIn => authToken != null;
+  
   @published
   String csrfToken;
   
-  String authToken = 'Authorization test';
+  AuthToken get authToken {
+    var c = _cookies['authToken'];
+    if (c == null)
+      return null;
+    return AuthToken.parse(c.value);
+  }
   
   SessionElement.created(): super.created() {
     this._cookies = new CookieJar(document.cookie);
-  }
-  
-  void attached() {
-    //TODO: Handle authentication
-    /*
-    var authTokenCookie = this._cookies['session_auth'];
-    if (authTokenCookie == null) {
-      //window.location.href = 'http://REDIRECT_TO_AUTH?cb=${window.location.href}';
-    }
-    this.authToken = UTF8.decode(CryptoUtils.base64StringToBytes(authTokenCookie.value)); 
-   */
   }
   
   SessionClient get httpClient => new SessionClient._(this);
@@ -63,9 +68,54 @@ class SessionClient extends Object with BaseClient {
   @override
   Future<StreamedResponse> send(BaseRequest request) {
     return _async.then((_) {
+      print('attching csrf token ${sessionElement.csrfToken}');
       request.headers['X-CSRFToken'] = sessionElement.csrfToken;
-      request.headers['Authorization'] = sessionElement.authToken;
+      
+      if (sessionElement.authToken != null) {
+        print('Attaching auth token ${sessionElement.authToken}');
+        request.headers['Authorization'] = '${sessionElement.authToken}';
+      }
       return _baseClient.send(request);
     });
   }
+}
+
+abstract class AuthToken {
+  static AuthToken parse(String rawToken) {
+    rawToken = rawToken.replaceAll('"', '');
+    var components = rawToken.split(' ');
+    if (components[0] == 'Basic') {
+      var userPass = UTF8.decode(CryptoUtils.base64StringToBytes(components[1]))
+          .split(':');
+      return new BasicAuthToken(userPass[0], userPass[1]);
+    } else {
+      throw new ParseError(0, 'Unrecognised authType: ${components[0]}');
+    }
+  }
+  
+  String get authType;
+  
+  String get token;
+}
+
+class BasicAuthToken implements AuthToken {
+  
+  final String authType = 'Basic';
+  
+  final String _username;
+  final String _password;
+  
+  BasicAuthToken(this._username, this._password);
+  
+  String get token =>
+      CryptoUtils.bytesToBase64(UTF8.encode('$_username:$_password'));
+  
+  String toString() => '$authType $token';
+}
+
+class ParseError extends StateError {
+  ParseError(int position, String message):
+    super('Parse error at $position: $message');
+  
+  toString() => 'ParseError: $message';
 }
