a1c2b9707849885654a78f02d52865580e6771ee
Thomas Stephenson <ovangle@gmail.com>
2014-12-15 00:40:23 +1100

    Added an input element which handles currencies

---
+++ lib/money_input/money_input.dart
@@ -0,0 +1,51 @@
+library cs_elements.money_input;
+
+import 'dart:html';
+import 'dart:js';
+import 'dart:math';
+
+import 'package:polymer/polymer.dart';
+
+import 'package:core_elements/core_input.dart';
+
+@CustomTag('cs-money-input')
+class MoneyInput extends PolymerElement {
+  
+  @published
+  String symbol;
+  
+  /// Display the symbol *after* the input element?
+  @published
+  bool appendSymbol;
+  
+  @published
+  int get numDigits => readValue(#numDigits, () => 2);
+  set numDigits(int value) => writeValue(#numDigits, value);
+  
+  @published
+  String name;
+  
+  @published
+  String label;
+  
+  @published
+  String value;
+  
+  @observable
+  String get committedValue => _input.committedValue;
+  set committedValue(String value) => _input.committedValue = value;
+  
+  @observable
+  int get inputStep => pow(10, -numDigits);
+  
+  CoreInput get _input => shadowRoot.querySelector('input[is=core-input]');  
+  
+  MoneyInput.created(): super.created();
+  
+  factory MoneyInput() => new Element.tag('input', 'money-input');
+  
+  void commit() {
+    this.committedValue = this.value;
+  }
+ 
+}
