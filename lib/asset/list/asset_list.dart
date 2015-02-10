4a431f791064786722c92ea595b576b0a1ce9490
Thomas Stephenson <ovangle@gmail.com>
2015-02-10 18:21:52 +1100

    Can delete items from an asset list

--- lib/asset/list/asset_list.dart
+++ lib/asset/list/asset_list.dart
@@ -5,6 +5,7 @@ import 'dart:html';
 
 import 'package:polymer/polymer.dart';
 import '../base/asset_list_base.dart';
+import 'asset_list_item.dart';
 
 @CustomTag('asset-list')
 class AssetList extends PolymerElement with AssetListBase {
@@ -23,5 +24,14 @@ class AssetList extends PolymerElement with AssetListBase {
   void createAsset(Event e) {
     e.preventDefault();
     window.location.href = '/asset/create';
+  }
+
+  void handleAssetDeleted([Event e]) {
+    print('asset deleted');
+    print((e.target as AssetListItem).asset);
+    this.assetList.assets.removeWhere(
+        (asset) => asset.id == (e.target as AssetListItem).asset.id
+    );
+
   }
 }
--- lib/asset/list/asset_list_item.dart
+++ lib/asset/list/asset_list_item.dart
@@ -1,19 +1,36 @@
 
 library cs_elements.asset_list_item;
 
+import 'dart:async';
 import 'dart:html';
 
 import 'package:polymer/polymer.dart';
+import 'package:core_elements/core_ajax_dart.dart';
 import '../base/asset_base.dart';
+import '../../session/session.dart';
 
 @CustomTag('asset-list-item')
 class AssetListItem extends PolymerElement with AssetBase {
 
-  AssetListItem.created(): super.created();
+  Stream get onAssetDeleted => on['asset-deleted'];
+
+  @observable bool deletingAsset;
+
+  SessionElement get session {
+    return document.querySelector('cs-session');
+  }
+
+  AssetListItem.created(): super.created() {
+    deletingAsset = false;
+  }
 
   @override
   void attached() {
     super.attached();
+    Polymer.onReady.then((_) {
+      //session = document.querySelector('cs-session');
+      //print(session.sessionHeaders);
+    });
   }
 
   @override
@@ -23,5 +40,19 @@ class AssetListItem extends PolymerElement with AssetBase {
 
   void navigateTo() {
     window.location.href = '/asset/${asset.id}';
+  }
+
+  void deleteAsset() {
+    CoreAjax coreAjax = shadowRoot.querySelector('core-ajax-dart');
+    HttpRequest request = coreAjax.go();
+    deletingAsset = true;
+
+    request.onLoad.first.then((_) {
+      if (request.status == 200) {
+        deletingAsset = false;
+        this.fire('asset-deleted', detail: this);
+      }
+    });
+
   }
 }
